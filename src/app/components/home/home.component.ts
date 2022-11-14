b09d507a2cfa4d4ccec60b219af3df7d30982bdd
kasturi28 <kasturipansare2818@gmail.com>
2022-11-14 19:43:16 +0530

    Added_Simple UI with console printing and connected to backend with all CRUD operations

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EmployeeServiceService } from 'src/app/employee-service.service';
+import { Employee } from 'src/app/model/employee.model';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +14,39 @@ export class HomeComponent implements OnInit {
   deleteUrl="./assets/icons/delete-black-18dp.svg";
   editUrl="./assets/icons/create-black-18dp.svg";
 
-  constructor() { }
+  constructor(
+    private router:Router,
+    private service:EmployeeServiceService
+  ) { }
+
+  list:any;
+  public employeeUpdate:Employee=new Employee("",0,"",new Date, "", "",[])
 
   ngOnInit(): void {
+    
+    this.service.getAllEmployee().subscribe((data:any) =>{
+      console.log(data);
+      this.list= data.data;
+    });
   }
 
-}
+    onDelete(id : Number){
+      console.log(id);
+      this.service.deleteEmployeeById(id).subscribe((data:any) =>{
+        console.log(data);
+        this.ngOnInit();
+      })
+    }
+     id:Number = 0;
+  
+    onUpdate(id:Number,employeeUp:Employee){
+      console.log(id,employeeUp)
+      this.service.updateDataFunc(id,employeeUp);
+      this.router.navigate(["form"]);
+      
+    }
+    
+  }
+
+
+
--- src/app/employee-service.service.ts
+++ src/app/employee-service.service.ts
@@ -1,20 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeServiceService {
 
-  constructor(private http:HttpClient) { }
+  id:Number = 0;
+  employeeUp:any;
+  constructor(private http:HttpClient,
+    ) { }
+
+  updateDataFunc(id:Number,employee:any){
+    this.id = id;
+    this.employeeUp = employee;
+  }
 
   insertEmployee(employee : any){
     return this.http.post("http://localhost:8085/create",employee);
   }
 
-  // getEmployee(employee : any){
-  //   return this.http.get("http://localhost:8085/get",employee);
-  // }
+   getAllEmployee():Observable<object>{
+     return this.http.get("http://localhost:8085/getAll");
+   }
+
+   deleteEmployeeById(id :Number){
+    return this.http.delete("http://localhost:8083/employeepayrollservice/delete/"+id)
+  }
+
+  updateById(id : Number,employee:any){
+    return this.http.put("http://localhost:8083/employeepayrollservice/update/"+id,employee)
+  }
+
+
+
+}
 
   
-}
